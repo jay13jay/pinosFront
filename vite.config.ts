557cc5c456e4abc5b32e7a85d2fdb5cc871b5970
jhax <josh.hackney13@gmail.com>
2025-03-01 14:31:15 -0500

    fixes baseURL

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/pinosFront/', // serve app at this base path
+  base: '/pinosFront', // serve app at this base path
   plugins: [
     react(),
     tailwindcss(),
