f2b3e1382f0feb637fe836652f62d996d688b996
jhax <josh.hackney13@gmail.com>
2025-03-01 16:25:02 -0500

    updates navbar elements so the transition between small and large screens is more fluid

--- src/Components/Navigation/Navbar2.tsx
+++ src/Components/Navigation/Navbar2.tsx
@@ -9,7 +9,9 @@ import {
     faPizzaSlice, 
     faHouse, 
     faMapLocationDot, 
-    faShoppingBasket
+    faShoppingBasket,
+    faBars,
+    faTimes
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from 'react-router-dom';
 
@@ -29,21 +31,21 @@ export function NavbarDefault({ title }: NavbarDefaultProps) {
 
   const navList = (
     <ul className="mt-2 mb-4 flex font-black flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-      <li className="flex items-center gap-x-2 p-1 font-medium hover:text-green-500">
+      <li className="flex items-center gap-x-2 p-1 font-medium text-green-500 hover:text-green-700">
         <FontAwesomeIcon icon={faHouse}/>
         <Link to="/" className="flex items-center">
           Home
         </Link>
       </li>
       <li className="flex items-center gap-x-2 p-1 font-medium">
-        <Link to="/menu" className="flex items-center gap-2 hover:text-yellow-500">
+        <Link to="/menu" className="flex items-center gap-2 text-gray-100 hover:text-gray-700">
           <FontAwesomeIcon icon={faPizzaSlice} />
           Menu
         </Link>
       </li>
       <li className="flex items-center gap-x-2 p-1 font-medium">
         <FontAwesomeIcon icon={faMapLocationDot} />
-        <Link to="/location-and-hours" className="flex items-center">
+        <Link to="/location-and-hours" className="flex items-center text-red-500 hover:text-red-700">
           Location & Hours
         </Link>
       </li>
@@ -58,22 +60,9 @@ export function NavbarDefault({ title }: NavbarDefaultProps) {
       onPointerLeaveCapture={undefined}>
 
       <div className="container mx-auto flex items-center justify-between text-black-900">
-        <Link to="/" className="mr-4 cursor-pointer py-1.5 font-medium text-white hover:text-yellow-500">
-          Pinos RVA
-        </Link>
-        <div className="hidden lg:block">{navList}</div>
-        <div className="flex items-center gap-x-1 lg:order-none order-1">
-          <li className="flex items-center gap-x-2 p-1 font-medium cursor-pointer text-white hover:text-yellow-500">
-            <FontAwesomeIcon icon={faShoppingBasket} />
-            <Link to="/cart" className="flex items-center">
-              Order
-            </Link>
-          </li>
-        </div>
-
         <IconButton
           variant="text"
-          className="ml-auto h-6 w-6 flex items-center justify-center hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+          className="mr-4 h-6 w-6 flex items-center justify-center lg:hidden"
           ripple={false}
           onClick={() => setOpenNav(!openNav)}
           placeholder={undefined}
@@ -81,36 +70,23 @@ export function NavbarDefault({ title }: NavbarDefaultProps) {
           onPointerLeaveCapture={undefined}
         >
           {openNav ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              className="h-6 w-6"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
+            <FontAwesomeIcon icon={faTimes} className="h-6 w-6 text-green-500" />
           ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
+            <FontAwesomeIcon icon={faBars} className="h-6 w-6 text-green-500" />
           )}
         </IconButton>
+        <Link to="/" className="mr-4 cursor-pointer py-1.5 font-medium text-gray-100 hover:text-gray-500 lg:text-green-500 lg:hover:text-green-700">
+          Pinos RVA
+        </Link>
+        <div className="hidden lg:block">{navList}</div>
+        <div className="flex items-center gap-x-1 lg:order-none order-1">
+          <li className="flex items-center gap-x-2 p-1 font-medium cursor-pointer text-red-500 hover:text-red-700">
+            <FontAwesomeIcon icon={faShoppingBasket} />
+            <Link to="/cart" className="flex items-center">
+              Order
+            </Link>
+          </li>
+        </div>
       </div>
       <MobileNav open={openNav}>
         <div className="container mx-auto">
